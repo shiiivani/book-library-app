ca40d8357bde9584a01c8cc7b6a2312ddd0395f0
Your Name <you@example.com>
2024-07-23 18:34:52 +0530

    Updated the UI

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
   Button,
@@ -9,6 +8,8 @@ import {
   Image,
   ScrollView,
   TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 
 export default function Homepage() {
@@ -18,6 +19,7 @@ export default function Homepage() {
   );
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredBooks, setFilteredBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -28,6 +30,8 @@ export default function Homepage() {
         setBooks(response.data.reading_log_entries);
       } catch (error) {
         console.error("Error fetching books:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -50,73 +54,121 @@ export default function Homepage() {
     setFilteredBooks(filtered);
   };
 
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color="blue"
+        style={{ marginTop: "95%" }}
+      />
+    );
+  }
+
   return (
     <ScrollView>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          onChangeText={handleSearch}
-          value={searchQuery}
-          placeholder="Search by title..."
-        />
-        {filteredBooks.map((filteredbook, index) => (
-          <View key={index} style={styles.bookItem}>
-            <Image
-              source={{
-                uri: `https://covers.openlibrary.org/b/id/${filteredbook.work.cover_id}-M.jpg`,
-              }}
-              style={styles.image}
-              resizeMode="cover"
-              alt="Image of a Book"
-            />
-            <View style={styles.info}>
-              <Text style={styles.title}>{filteredbook.work.title}</Text>
-              <Text style={styles.author}>
-                Author: {filteredbook.work.author_names.join(", ")}
-              </Text>
-              <Text style={styles.year}>
-                {filteredbook.work.first_publish_year}
-              </Text>
-            </View>
-            <Button
-              title={buttonStates[index] ? "Read" : "Unread"}
-              onPress={() => handleButton(index)}
-              style={styles.button}
-              color={buttonStates[index] ? "green" : "grey"}
-            />
-          </View>
-        ))}
-
-        {books.map(
-          (book, index) =>
-            book.work.title && (
-              <View key={index} style={styles.bookItem}>
-                <Image
-                  source={{
-                    uri: `https://covers.openlibrary.org/b/id/${book.work.cover_id}-M.jpg`,
-                  }}
-                  style={styles.image}
-                  resizeMode="cover"
-                  alt="Image of a Book"
-                />
-                <View style={styles.info}>
-                  <Text style={styles.title}>{book.work.title}</Text>
-                  <Text style={styles.author}>
-                    Author: {book.work.author_names.join(", ")}
-                  </Text>
-                  <Text style={styles.year}>
-                    {book.work.first_publish_year}
-                  </Text>
-                </View>
-                <Button
-                  title={buttonStates[index] ? "Read" : "Unread"}
-                  onPress={() => handleButton(index)}
-                  style={styles.button}
-                  color={buttonStates[index] ? "green" : "grey"}
-                />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleSearch}
+            value={searchQuery}
+            placeholder="Search by title..."
+          />
+          {filteredBooks.map((filteredbook, index) => (
+            <View key={index} style={styles.bookItem}>
+              <Image
+                source={{
+                  uri: `https://covers.openlibrary.org/b/id/${filteredbook.work.cover_id}-M.jpg`,
+                }}
+                style={styles.image}
+                resizeMode="cover"
+                alt="Image of a Book"
+              />
+              <View style={styles.info}>
+                <Text style={styles.title}>{filteredbook.work.title}</Text>
+                <Text style={styles.author}>
+                  Author: {filteredbook.work.author_names.join(", ")}
+                </Text>
+                <Text style={styles.year}>
+                  {filteredbook.work.first_publish_year}
+                </Text>
               </View>
-            )
-        )}
+              <TouchableOpacity
+                onPress={() => handleButton(index)}
+                style={[
+                  styles.button,
+                  {
+                    borderWidth: buttonStates[index] ? 0 : 1,
+                    backgroundColor: buttonStates[index]
+                      ? "green"
+                      : "transparent",
+                  },
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.buttonText,
+                    { color: buttonStates[index] ? "white" : "black" },
+                  ]}
+                >
+                  {buttonStates[index] ? "Read" : "Unread"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+
+          {books.map(
+            (book, index) =>
+              book.work.title && (
+                <View key={index} style={styles.bookItem}>
+                  <Image
+                    source={{
+                      uri: `https://covers.openlibrary.org/b/id/${book.work.cover_id}-M.jpg`,
+                    }}
+                    style={styles.image}
+                    resizeMode="cover"
+                    alt="Image of a Book"
+                  />
+                  <View style={styles.info}>
+                    <Text style={styles.title}>{book.work.title}</Text>
+                    <Text style={styles.author}>
+                      Author: {book.work.author_names.join(", ")}
+                    </Text>
+                    <Text style={styles.year}>
+                      {book.work.first_publish_year}
+                    </Text>
+                  </View>
+                  <TouchableOpacity
+                    onPress={() => handleButton(index)}
+                    style={[
+                      styles.button,
+                      {
+                        borderWidth: buttonStates[index] ? 0 : 1,
+                        backgroundColor: buttonStates[index]
+                          ? "green"
+                          : "transparent",
+                      },
+                    ]}
+                  >
+                    <Text
+                      style={[
+                        styles.buttonText,
+                        { color: buttonStates[index] ? "white" : "black" },
+                      ]}
+                    >
+                      {buttonStates[index] ? "Read" : "Unread"}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )
+          )}
+        </View>
       </View>
     </ScrollView>
   );
@@ -145,21 +197,19 @@ const styles = StyleSheet.create({
   },
   bookItem: {
     width: "45%",
-    height: 560,
-    marginTop: 20,
-    borderWidth: 0.5,
-    borderColor: "black",
+    height: "max-content",
+    borderWidth: 0.4,
+    borderColor: "grey",
     alignItems: "left",
     padding: 10,
   },
   title: {
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: "bold",
     marginTop: 5,
   },
   author: {
     marginTop: 5,
-    // fontWeight: "bold",
     fontSize: 13,
     color: "grey",
   },
@@ -176,9 +226,16 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   info: {
-    height: 225,
+    height: "max-content",
   },
   button: {
     marginTop: 10,
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "100%",
+  },
+  buttonText: {
+    textAlign: "center",
+    padding: 8,
   },
 });
